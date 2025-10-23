c670eb2a2955cd724771231e6d3f2369a4f4e25f
落宇 <99511204@qq.com>
2025-10-23 15:27:13 +0800

    处理掉lobe chat标识，标识可自定义

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
+export const LOBE_CHAT_CLOUD = '易商AI Cloud';
 
-export const BRANDING_NAME = 'LobeHub';
+export const BRANDING_NAME = '易商AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '易商AI';
 
 export const BRANDING_URL = {
   help: undefined,
